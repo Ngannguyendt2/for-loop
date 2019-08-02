9d9a9b4e28e72becec5bca137969f6279486c745
= <=>
2019-08-02 13:34:15 +0700

    up file use for loop

---
+++ bai1.js
@@ -0,0 +1,10 @@
+function bai1(){
+    let i;
+    let text="";
+    for(i=0;i<=5;i++)
+    {
+        text+="This is"+i+"<br>" ;
+
+        document.getElementById('text').innerHTML=text;
+    }
+}
---
+++ bai2.js
@@ -0,0 +1,10 @@
+function bai2()
+{
+    let sum=0;
+    let num=parseInt(prompt("Your number: "));
+    for(let i=1;i<=num;i++)
+    {
+        sum=sum+i;
+    }
+    alert('Sum= '+sum);
+}
